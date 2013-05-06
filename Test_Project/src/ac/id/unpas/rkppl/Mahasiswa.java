88f64f4a52b9b24aecca3d4f0efde895f3eb5cb0
rkkpplpdc <andiasraf_hamid@unpas.ac.id>
2013-05-06 14:33:53 +0700

    create class mahasiswa

---
+++ Test_Project/src/ac/id/unpas/rkppl/Mahasiswa.java
@@ -0,0 +1,20 @@
+package ac.id.unpas.rkppl;
+
+public class Mahasiswa {
+
+	private String mhs;
+	
+	public String getMhs() {
+		return mhs;
+	}
+
+	public void setMhs(String mhs) {
+		this.mhs = mhs;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
